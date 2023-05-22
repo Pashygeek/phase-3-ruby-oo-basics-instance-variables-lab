29ac464624af6927b96cde94c98896a334f03230
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-05-22 12:52:45 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    def name=(dog_name)
+      @this_dogs_name = dog_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+  
+# New dog object
+my_dog = Dog.new
 
-end
+# Name of the dog
+my_dog.name = "Buddy"
+
+#Retrieve name of the dog
+puts my_dog.name
